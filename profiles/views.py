5d3152f1bca6eb70cc05672aef0b43467e674632
Didisimmons <onyema.onyejekwe@gmail.com>
2022-02-11 02:04:17 +0000

    add order history to profile.html and add profile link to checkout success.html

--- profiles/views.py
+++ profiles/views.py
@@ -4,6 +4,9 @@ from django.contrib import messages
 from .models import UserProfile
 from .forms import UserProfileForm
 
+from checkout.models import Order
+
+
 def profile(request):
     """ Display the user's profile. """
     profile = get_object_or_404(UserProfile, user=request.user)  # Get the profile for the current user. And then return it to the template.
@@ -30,4 +33,20 @@ def profile(request):
 
     return render(request, template, context)
 
-    
+def order_history(request, order_number):
+    order = get_object_or_404(Order, order_number=order_number)  # retrieve order
+
+    # message letting the user know they're looking at a past order confirmation.
+    messages.info(request, (
+        f'This is a past confirmation for order number {order_number}. '
+        'A confirmation email was sent on the order date.'
+    ))
+
+    template = 'checkout/checkout_success.html'
+    # template for rendering order confirmation.
+    context = {
+        'order': order,
+        'from_profile': True,  # used to check in that template if the user got there via the order history view.
+    }
+
+    return render(request, template, context)
