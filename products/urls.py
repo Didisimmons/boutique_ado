5d83862e2f23e8d42df682853f2e3daebc368119
Didisimmons <onyema.onyejekwe@gmail.com>
2022-02-12 03:21:40 +0000

    added product view, url and template to allow admin add products

--- products/urls.py
+++ products/urls.py
@@ -3,5 +3,6 @@ from . import views
 
 urlpatterns = [
     path('', views.all_products, name='products'),
-    path('<product_id>', views.product_detail, name='product_detail'),
+    path('<int:product_id>/', views.product_detail, name='product_detail'),  # to specify that the product ID should be an integer.
+    path('add/', views.add_product, name='add_product'),
 ]
